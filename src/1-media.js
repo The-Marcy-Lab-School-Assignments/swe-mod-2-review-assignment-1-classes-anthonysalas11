34ce46061a8d8189a53396fd2f2e28c77e400111
anthonysalas11 <anthony.salas.ts@gmail.com>
2025-12-18 00:19:09 -0500

    progress made

--- src/1-media.js
+++ src/1-media.js
@@ -1,11 +1,25 @@
 // Problem 1: Inheritance and Polymorphism - Media Player
 // Implement your MediaItem, Song, Podcast, and Audiobook classes below
 class MediaItem {
+    constructor(title, duration){
+        this.title = title
+        this.duration = duration
+    }
+
+    play() {
+        return `playing: ${this.title}`
+
+    }
+
+    getFormattedDuration() {
+        return `${this.duration}`
+    }
 
 }
 
-class Song {
 
+class Song {
+   
 }
 
 class Podcast {
--- src/2-team.js
+++ src/2-team.js
@@ -2,10 +2,45 @@
 // Implement your Player and Team classes below
 
 class Player {
+constructor(name , position, jerseyNumber){
+    this.name = name
+    this.position = position
+    this.jerseyNumber = jerseyNumber
+    }
+}
+
+
+
+class Team {
+
+    #starters = []
+    #bench = []
+
+    constructor(name){
+        this.name = name 
+    }
+
+    get Starters() {
+        return [...this.#starters]
+}
+
+    get Bench() {
+        return [...this.#bench]
+}
+    addBenchPlayer(player) {
+        this.#bench.push(player)
+}
+    getPlayerCount() {
+    return this.#starters + this.#bench
+}
+    moveToBench(name) {
+
+}
+    moveToStarters(name) {
 
 }
 
-class Team {
+
 
 }
 
